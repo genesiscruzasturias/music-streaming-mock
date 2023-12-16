bc8485542774c433fb8bddd9963473dbe08a7c5a
Genesis Cruz <genesis.cruzasturias@gmail.com>
2023-12-16 16:17:27 -0600

    commit

---
+++ src/main/java/com/music/musicstreamingmock/MusicStreamingMockApplication.java
@@ -0,0 +1,13 @@
+package com.music.musicstreamingmock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicStreamingMockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicStreamingMockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/music/musicstreamingmock/MusicStreamingMockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.music.musicstreamingmock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicStreamingMockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
